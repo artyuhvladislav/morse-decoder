43bf07f35f7b21aec6bb8385993ced3c925498e8
artyuhvladislav <workto22@gmail.com>
2019-10-09 19:05:59 +0300

    feat: add morse task

--- src/index.js
+++ src/index.js
@@ -38,8 +38,27 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+   let result = '';
+   for(let i = 0; i<expr.length; i += 10) {
+    let letter = '';
+    if(expr[i] === '*') {
+        result += ' ';
+    }
+    else {
+        for(let n = i; n < (i+10); n += 2) {
+            if(+expr[n] === 1 && +expr[n+1] === 0) {
+                letter+='.';
+            }
+            if(+expr[n] === 1 && +expr[n+1] === 1) {
+                letter += '-';
+            }
+        }
+        result += MORSE_TABLE[letter];
+    }
 }
+return result;
+}
+
 
 module.exports = {
     decode
